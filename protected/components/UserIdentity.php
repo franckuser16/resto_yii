ea3785d3b63b70a90f8cfee449bab2963f683ea6
Franck Michaud <franckuser16@gmail.com>
2012-01-25 20:25:00 +0100

    commented phpinfo and changed try catch in UserIdentity

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -17,39 +17,43 @@ class UserIdentity extends CUserIdentity
 		 */
 		public function authenticate()
 		{
-		/*
-		$users=array(
-			// username => password
-			'user'=>'tpinfo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		else if($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
-		 */
-				ini_set('display_errors', 'On');
-				error_reporting(E_ALL);
+				/*
+				$users=array(
+					// username => password
+					'user'=>'tpinfo',
+					'admin'=>'admin',
+				);
+				if(!isset($users[$this->username]))
+					$this->errorCode=self::ERROR_USERNAME_INVALID;
+				else if($users[$this->username]!==$this->password)
+					$this->errorCode=self::ERROR_PASSWORD_INVALID;
+				else
+					$this->errorCode=self::ERROR_NONE;
+				return !$this->errorCode;
+				 */
+				try{
+						$connection = ldap_connect(Yii::app()->params['ldap_host'], Yii::app()->params['ldap_port']);
 
-				$connection = ldap_connect(Yii::app()->params['ldap_host'], Yii::app()->params['ldap_port']);
-				ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, 3);
-				ldap_set_option($connection, LDAP_OPT_REFERRALS, 0);
+						ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, 3);
+						ldap_set_option($connection, LDAP_OPT_REFERRALS, 0);
 
-				if ($connection) {
+						if ($connection) {
 
-						//try catch
-						$bind = ldap_bind($connection, $this->username.'@'.Yii::app()->params['ldap_domain'], $this->password);
-						if ($bind) {
-								$this->errorCode=self::ERROR_NONE;
-						} 
+								$bind = ldap_bind($connection, $this->username.'@'.Yii::app()->params['ldap_domain'], $this->password);
+								if ($bind) {
+										$this->errorCode=self::ERROR_NONE;
+								} 
+								else {
+										$this->errorCode=self::ERROR_UNKNOWN_IDENTITY;
+								}
+						}
 						else {
+								//a changer pour une erreur LDAP connexion
 								$this->errorCode=self::ERROR_UNKNOWN_IDENTITY;
 						}
 				}
-				else {
+				catch (Exception $error){
+
 						//a changer pour une erreur LDAP connexion
 						$this->errorCode=self::ERROR_UNKNOWN_IDENTITY;
 				}
